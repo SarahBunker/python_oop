270b1ee49bcc923dd671ab54c9e952eae335e22f
Sarah Bunker <ChemEnCoder@gmail.com>
2023-05-01 17:47:49 -0600

    review of creating classes and instantiating objects from classes in python

---
+++ classes_objects.py
@@ -0,0 +1,45 @@
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+
+print(p1.x)
+
+'''
+init function
+- initializing different values in class
+'''
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+# instantiating object from class
+
+me = Person("Sarah", 30)
+
+print(me.name)
+print(me.age)
+
+'''
+delete property on object
+'''
+del me.age
+
+# print(me.age) # AttributeError: 'Person' object has no attribute 'age'
+
+'''
+deleting object
+'''
+del me
+
+# print(me) #NameError: name 'me' is not defined
+
+'''
+empty class
+bypass error using pass
+'''
+
+class People:
+  pass
