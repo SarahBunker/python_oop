4cfae64851a7aad72be06441c44f28cc9c3d0c13
Sarah Bunker <ChemEnCoder@gmail.com>
2023-05-01 17:53:41 -0600

    simple inheritance example

--- inheritance.py
+++ inheritance.py
@@ -1,4 +1,11 @@
 from classes_objects import Student
 
-tim = Student();
+tim = Student()
 print(tim.name)
+
+# Person inherits from Student class
+class Person(Student):
+  pass
+
+p1 = Person()
+print(p1.name)
